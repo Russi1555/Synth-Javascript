208668b62f192e18fc6a26fc30ae1ec951012750
Gustavo Russi <78427180+Russi1555@users.noreply.github.com>
2024-01-28 12:41:32 -0300

    Osciloscópio funcional
    Ainda da pra melhorar, se deixar um intervalo fixo ao invés de ficar correndo atrás da onde vai ficar mais visível

--- main.js
+++ main.js
@@ -1,18 +1,99 @@
 const audioContext = new AudioContext();
+
+function makeDistortionCurve(amount) {
+    const samples = 44100;
+    const curve = new Float32Array(samples);
+    const deg = Math.PI / 180;
+
+    for (let i = 0; i < samples; ++i) {
+        const x = (i * 2) / samples - 1;
+        curve[i] = Math.sin(Math.PI * amount * x) * 0.75; // Experiment with this expression
+    }
+
+    return curve;
+}
+
+const Nodo_Distortion = audioContext.createWaveShaper(); //Nodo de distorção não linear
+Nodo_Distortion.curve = makeDistortionCurve(1);
+Nodo_Distortion.oversample = '4x';
+const Nodo_ganho = audioContext.createGain(); //Nodo de controle de ganho
+const biquadFilter = audioContext.createBiquadFilter(); //Nodo de filtro
+
+//Nodo que aplica "Linear Convolution" https://en.wikipedia.org/wiki/Convolution#Visual_explanation Comum pra efeito de reverb
+const convolver = audioContext.createConvolver(); 
+
+
 const Osc_ativos = [];
-const Nodo_ganho = audioContext.createGain();
-Nodo_ganho.connect(audioContext.destination);
+
+
+Nodo_ganho.connect(Nodo_Distortion);
+
+const analyser = audioContext.createAnalyser(); // Nodo de análise, entra e sai sem alteração
+analyser.fftSize = 2048;
+analyser.minDecibels = -90;
+analyser.maxDecibels = -10;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+analyser.smoothingTimeConstant = 0.85;
+// Connect the source to be analysed
+Nodo_Distortion.connect(analyser);
+analyser.connect(audioContext.destination);
+
+// Get a canvas defined with ID
+const canvas = document.getElementById("grafico_final");
+const canvasCtx = canvas.getContext("2d");
+
 
 const volumeControl = document.querySelector("input[name='volume']");
+const DistortionControl = document.querySelector("input[name='distInput']");
 const oitavaInput = document.getElementById("oitavaInput");
 
+function draw() {
+    requestAnimationFrame(draw);
+  
+    analyser.getByteTimeDomainData(dataArray);
+  
+    canvasCtx.fillStyle = "rgb(200 200 200)";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "rgb(0 0 200)";
+  
+    canvasCtx.beginPath();
+  
+    const sliceWidth = (canvas.width * 1.0) / bufferLength;
+    let x = 0;
+  
+    for (let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = (v * canvas.height) / 2;
+  
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+  
+      x += sliceWidth;
+    }
+  
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
+  
+  draw();
+  
 let noteFreq = [{}];
 
 const wavePicker = document.querySelector("select[name='waveform']");
-const customWaveform = audioContext.createPeriodicWave(new Float32Array([0, 0, 1, 0, 1]), new Float32Array([0, 0, 1, 0, 1]));
+const customWaveform = audioContext.createPeriodicWave([0, 0.5, 0.2, 0.1], [0, 0, 0, 0] );
+
 
 function setup() {
     volumeControl.addEventListener("change", changeVolume, false);
+    DistortionControl.addEventListener("change", changeDistortion, false);
+
     for (let i = 0; i < 9; i++){ //gera as notas
             noteFreq[i] = {
             "C": 16.35  *(2**i),
@@ -37,11 +118,29 @@ function setup() {
         
 
 function changeVolume(event) {
-    Nodo_ganho.gain.value = volumeControl.value;
+    const volumeValue = parseFloat(volumeControl.value);
+    Nodo_ganho.gain.value = volumeValue;
 }
+
+
+function changeDistortion(event) {
+    const distortionValue = parseFloat(DistortionControl.value) | 0 ;
+
+    if (distortionValue <2) {
+        // If distortion value is 0, set a default curve with no distortion
+        Nodo_Distortion.curve = makeDistortionCurve(1); // Use a small non-zero value
+    } else {
+        Nodo_Distortion.curve = makeDistortionCurve(distortionValue);
+    }
+
+    console.log("mudou algo");
+    console.log(distortionValue);
+}
+
 
 function nota(note) {
     const oitava = parseInt(oitavaInput.value) || 0;
+
     if (!Osc_ativos[oitava][note]) {
         Osc_ativos[oitava][note] = play_nota(oitava, note);
     }
@@ -72,3 +171,4 @@ function nota_release(note) {
 
 
 setup();
+changeVolume(0);
